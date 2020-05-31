cf3274f76159591347a750424803c6c000601d92
José Tomás Ahumada <jtahumada@miuandes.cl>
2020-05-30 23:34:54 -0400

    classes added

---
+++ Lab 8/Lab 8/Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Cinema
+    {
+        private string ownerName;
+        private string id;
+        private string attention;
+        private int rooms;
+    }
+}
---
+++ Lab 8/Lab 8/Store.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8
+{
+    public class Store
+    {
+        private string ownerName;
+        private string id;
+        private string attention;
+        private List<string> categories;
+    }
+}
